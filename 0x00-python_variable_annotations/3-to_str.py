75540452f95ed0e2d46099b6917b05fbae2f60d1
benkivuva <benkivuvak5@gmail.com>
2023-12-07 04:15:55 -0800

    add: Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Converts a float to its string representation."""
+
+
+def to_str(n: float) -> str:
+    """Converts a float to its string representation."""
+    return str(n)
