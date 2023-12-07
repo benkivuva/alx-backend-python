602e2cc5457d685de85336c85996b13f06f8c629
benkivuva <benkivuvak5@gmail.com>
2023-12-07 04:50:54 -0800

    Complex types - functions

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,6 +3,7 @@
 
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Converts a string and an int or float to a tuple."""
     return k, float(v ** 2)
---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Returns a function that multiplies a float by a given multiplier."""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Returns a function that multiplies a float by a given multiplier."""
+    def multiplier_function(x: float) -> float:
+        return x * multiplier
+    return multiplier_function
