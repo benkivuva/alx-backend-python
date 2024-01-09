f4120cee440dc6517ba6d8ebe7c02c571ee101c2
benkivuva <benkivuvak5@gmail.com>
2024-01-09 00:32:26 -0800

    update PEP8

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,44 +1,36 @@
 #!/usr/bin/env python3
-""" This project module contains unit tests for utils.access_nested_map.
+""" This project module contains the first unit test
+for utils.access_nested_map.
 """
-from parameterized import parameterized
+
 import unittest
-from typing import Mapping, Sequence
+from utils import access_nested_map
+from parameterized import parameterized
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """ TestAccessNestedMap inherits from unittest.TestCase to test
     utils.access_nested_map.
     """
-
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(
-        self, nested_map: Mapping, path: Sequence, expected_result
-    ):
-        """ Method to test that the method returns what it is supposed to.
+    def test_access_nested_map(self, nested_map, path, expected_result):
+        """ Tests if access_nested_map method returns what it is supposed to.
         """
-        self.assertEqual(
-            access_nested_map(nested_map, path), expected_result
-        )
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
     @parameterized.expand([
-        ({}, ("a",), KeyError, "Key not found: 'a'"),
-        ({"a": 1}, ("a", "b"), KeyError, "Key not found: 'b'"),
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
     ])
-    def test_access_nested_map_exception(
-        self, nested_map: Mapping, path: Sequence, expected_exception, expected_message
-    ):
-        """ Method to test that a KeyError is raised with the expected message.
+    def test_access_nested_map_exception(self, nested_map, path):
+        """ Tests if access_nested_map method raise an exception.
         """
-        with self.assertRaises(expected_exception) as context:
-            access_nested_map(nested_map, path)
-
-        self.assertEqual(str(context.exception), expected_message)
+        self.assertRaises(KeyError, access_nested_map, nested_map, path)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
